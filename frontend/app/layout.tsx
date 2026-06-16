5be7bacc8773ae2209e5dbbe8f2a981617740e41
Adetunji Samuel <samadet0001@gmail.com>
2026-06-16 05:23:06 +0100

    Update layout.tsx

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -7,7 +7,7 @@ import Footer from '../app/components/layout/Footer'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'CLUTCH AI | Turn Meetings into Action',
+  title: 'APEX AI | Turn Meetings into Action',
   description:
     'Extract summaries, decisions, and action items from your meetings instantly.',
 }
